80685d9d63de9c21646de94d38ed3411f6cab510
myroslav <myrkozar@gmail.com>
2022-05-24 13:08:40 +0300

    щ

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,7 @@
-import user from './user';
+import user from './Profile-user/user';
 
 import Section from './Section';
-import Profile from './Profile';
-
+import Profile from './Profile-user/Profile';
 
 export const App = () => {
   return (
@@ -13,17 +12,19 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       <Section title="Профиль социальной сети">
-        <Profile  username={user.username}
-                  tag={user.tag}
-                  location={user.location}
-                  avatar={user.avatar}
-                  followers={user.stats.followers}
-                  views={user.stats.views}
-                  likes={user.stats.likes}  />
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          followers={user.stats.followers}
+          views={user.stats.views}
+          likes={user.stats.likes}
+        />
       </Section>
     </div>
   );
